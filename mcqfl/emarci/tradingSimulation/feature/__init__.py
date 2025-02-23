7b9cd46b6be67161d2ee393df790ed60886f7b72
Max R. Chellew <110368632+MaxChellew@users.noreply.github.com>
2025-02-23 14:57:41 +0000

    Delete mcqfl directory

--- mcqfl/emarci/tradingSimulation/feature/__init__.py
+++
@@ -1,9 +0,0 @@
-#######################################################
-
-# Author: Max Chellew
-# Libary: emarci
-# File Path: emarci/tradingSimulation/feature/__init__.py
-
-#######################################################
-
-# imports (standard)
